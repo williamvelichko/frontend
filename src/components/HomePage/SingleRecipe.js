58236c354702bdb3bcd79b741cc0f071ce945e79
Jorge <jorgeevangelista500@gmail.com>
2022-02-02 21:25:53 -0500

    changed edit form to in component editing

--- src/components/HomePage/SingleRecipe.js
+++ src/components/HomePage/SingleRecipe.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function SingleRecipe(props) {
   const { recipe, handleEditSelect } = props;
+
+  const [ editing, setEditing ] = useState(false)
+ 
+  const [state, setState] = useState({
+    source: recipe.source,
+    ingredients: recipe.ingredients,
+    instructions:recipe.instructions,
+    category: recipe.category
+  });
+
+  const handleChange = (e) => {
+    console.log(e)
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+  
+  const handleCancel = () => {
+    setEditing(!editing)
+    setState({
+    source: recipe.source,
+    ingredients: recipe.ingredients,
+    instructions:recipe.instructions,
+    category: recipe.category
+    })
+  }
 
   return (
     <div>
       <h2>{recipe.title}</h2>
       <p>
-        <strong>Source:</strong> {recipe.source}
+        <strong>Source: </strong>{editing ?<input onChange={handleChange} name='source' type='text'  value={state.source}/>  : recipe.source}
       </p>
       <h4>
-        <strong>ingredients: {recipe.ingredients}</strong>
+        <strong>Ingredients:</strong> {editing ?<input onChange={handleChange} name='ingredients' type='text' value={state.ingredients}/>  : recipe.ingredients}
       </h4>
       <h4>
-        <strong>Instructions: {recipe.instructions}</strong>
+        <strong>Instructions:</strong> {editing ?<input onChange={handleChange} name='instructions' type='text' value={state.instructions}/>  : recipe.instructions}
       </h4>
       <h4>
-        <strong>Category: {recipe.category}</strong>
+        <strong>Category:</strong> {editing ?<input onChange={handleChange} name='category' type='text' value={state.category}/>  : recipe.category}
       </h4>
-      <button onClick={() => handleEditSelect(recipe.id)}>Edit</button>
+      <button onClick={() => handleCancel()}>{editing ? 'Cancel' : "Edit"}</button>
+      {editing && <button>Save</button>}
     </div>
   );
 }
